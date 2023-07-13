c34fb8c80505e20193c3d6726adddfa0257d8002
Zumh <17211423+Zumh@users.noreply.github.com>
2023-07-13 11:16:49 -0400

    clean up

--- index.js
+++ index.js
@@ -39,43 +39,6 @@ morgan.token("data", (request) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 
-// this is for default format
-//app.use(morgan('tiny'));
-
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
-
-
-	
-
-
-
-/*
-// 3.1 Node application that returns a hardcode list of phonebook entries from the address
-app.get('/api/persons', (request, response) => {
-	response.json(persons);
-});
-*/
 
 app.get('/api/persons', (request, response) => {
 	
@@ -85,13 +48,7 @@ app.get('/api/persons', (request, response) => {
 	
 });
 
-// 3.2 request was recieved and how many entries are in the phone book
-/*app.get('/info', (request, response) => {
-	// request was recieved 
-	// how many entires 
-	response.send(
-	`<br>Phonebook has info ${persons.length} people</br><br>${new Date()}</br>`);
-});*/
+
 // 3.18 info handling for updates 
 app.get('/info', (request, response, next) => {
 	Person.find({})
@@ -103,23 +60,7 @@ app.get('/info', (request, response, next) => {
 	.catch(error => next(error));
 });
 
-//3.3 functionality for displaying the information for single phonebook entry.
-//if not found then send status code 
-/*app.get('/api/persons/:id', (request, response) => {
-	// get the id number from the url or params string and convert it to integer
-	const id = Number(request.params.id);
 
-	// find the person 
-	const person = persons.find(person => person.id == id);
-
-	if (person) {
-		response.json(person);
-	} else {
-		response.status(404).end();
-	}
-
-});
-*/
 //3.18 displaying one person info at a time using :id
 app.get('/api/persons/:id', (request, response, next) => {
 
@@ -151,18 +92,7 @@ app.put("/api/persons/:id", (request, response, next) => {
 
 });
 
-// 3.4 functionality that make it possible to delete a single phonebook entry by HTTP DELETE
-/*app.delete('/api/people/:id', (request, response) => {
-	// find the person and delete 
-	const id = Number(request.params.id);
 
-	// simply regenerate a new list of persons here 
-	// let the garbage collector taker of deleting object 
-	persons = persons.filter(person => person.id != id);
-
-	response.status(204).end();
-});
-*/
 // 3.15: delete phone data from database 
 app.delete('/api/persons/:id', (request, response, next) => {
 	Person.findByIdAndRemove(request.params.id)
@@ -174,34 +104,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 
 
-// 3.5 functionality for adding new phonebook entries to http://localhost:3001/api/persons.
-/*
-app.post('/api/people', (request, response) => {
-	const body = request.body;
-	let message = "";
-	// if body is missing content then return rerror message 
-	if (!body.name){
-		message = "name is missing";
-	} else if (!body.number){
-		message = "number is missing";
-	} else if ( persons.find(person => person.name == body.name)){
-		message = "name already exists";
-	} 
-	if (message){
-		
-		return response.status(400).json({
-			error: message
-		});
 
-	} else {
-		// prepare person info
-		const person = {...body, id: generatedId()};
-		persons = persons.concat(person);
-		response.json(body);		
-	}
-
-});
-*/
 
 // new entry: save input data from text inbox to database 
 app.post('/api/persons', (request, response, next) => {
@@ -217,32 +120,11 @@ app.post('/api/persons', (request, response, next) => {
 	.catch((errorMessage) => next(errorMessage));
 });
 
-// generated id number for persons
-/*
-const generatedId = () => {
-	const maxId = (persons.length > 0 ? Math.max(...persons.map(person => person.id)): 0) + 1;
 
-	
-	const minId = persons.length > 0 ? Math.min(...persons.map(person => person.id)): 0;
-	var randomId = 0;
-	do {
-		randomId = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
-	} while (persons.map(person => person.id).includes(randomId));
-	return randomId;
-
-}
-*/
-
-/*
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
-*/
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3001; 
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`);
 });
--- models/person.js
+++ models/person.js
@@ -1,20 +1,6 @@
 // import mongoose library
 const mongoose = require('mongoose');
 
-// check if only password then show all the data 
-/*
-if (process.argv.length < 3){
-	console.log('give password as arugment');
-	process.exit(1);
-} 
-*/
-
-// node mongo.js password name number
-// node mongo.js password
-// we can extract password here
-//const password = process.argv[2]
-
-// get the uri 
 
 
 // make it less strict with Query for simplicity sake
@@ -65,40 +51,6 @@ personSchema.set('toJSON', {
 	} 
 });
 
-// create the person schema 
-// Person == persons from MongoDBAtlas 
-/*
-const Person = mongoose.model('Person', personSchema);
 
-if (process.argv.length > 2) {
-
-	const inputName = process.argv[2];
-	const inputNumber = process.argv[3];
-	
-	// we have to set up the data
-	const newPerson = new Person({
-		name: inputName,
-		number: inputNumber
-	});
-	// save the data and close connection 
-	newPerson.save().then(result => {
-		console.log(`added ${inputName} ${inputNumber} to phonebook`);
-
-		
-		mongoose.connection.close();
-	});
-
-} else {
-	console.log('phonebook:');
-	// extract all the info from database
-	Person.find({}).then(persons => {
-		persons.forEach( person => {
-			console.log(`${person.name} ${person.number}`);
-		});
-		mongoose.connection.close();
-	});
-}
-
-*/
 
 module.exports = mongoose.model('persons', personSchema);
